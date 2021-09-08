0ca7abdb9fd213a0a06a2c08a4016470f9202e86
natclark <39421782+natclark@users.noreply.github.com>
2021-09-08 18:39:30 -0500

    add new font, Inter var

--- src/lib/components/Navbar/index.svelte
+++ src/lib/components/Navbar/index.svelte
@@ -79,7 +79,7 @@
                     color: #222;
                     display: flex;
                     font-size: 20px;
-                    font-weight: 700;
+                    font-weight: 600;
                     height: 40px;
                     overflow: hidden;
                     padding: 0 8px;
