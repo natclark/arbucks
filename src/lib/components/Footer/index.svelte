4f1da99040c9d58963cb60cef6e91f055c4a8256
natclark <39421782+natclark@users.noreply.github.com>
2021-09-08 14:39:54 -0500

    add privacy-friendly analytics for displaying trending tickers/tokens

--- src/lib/components/Footer/index.svelte
+++ src/lib/components/Footer/index.svelte
@@ -4,6 +4,9 @@
     <p>Please don't shoot the messenger!</p>
 </footer>
 
+<!-- This is a privacy-friendly analytics solution. The analytics dashboard is public. This is for displaying trending tickers and popular tokens. A more decentralized solution should be implemented in the future, however. -->
+<script type="text/javascript" src="https://api.arbucks.io/latest.js" async defer></script>
+
 <style>
     footer {
         background-color: rgb(239, 222, 205, .6);
