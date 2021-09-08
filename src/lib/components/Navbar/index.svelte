0782e957e6a58bed28ffd4aa3adf9b6893cf415d
natclark <39421782+natclark@users.noreply.github.com>
2021-09-08 00:32:41 -0500

    add home icon to navbar

--- src/lib/components/Navbar/index.svelte
+++ src/lib/components/Navbar/index.svelte
@@ -1,7 +1,13 @@
 <nav>
     <div class="container">
         <div class="left">
-            <a href="/" sveltekit:prefetch>Arbucks</a>
+            <a href="/" title="Home" sveltekit:prefetch>
+                <svg width="28px" height="28px" version="1.1" x="0px" y="0px" viewBox="0 0 122.88 112.07" style="enable-background:new 0 0 122.88 112.07" xml:space="preserve">
+                    <g>
+                        <path clip-rule="evenodd" fill-rule="evenodd" d="M61.44,0L0,60.18l14.99,7.87L61.04,19.7l46.85,48.36l14.99-7.87L61.44,0L61.44,0z M18.26,69.63L18.26,69.63 L61.5,26.38l43.11,43.25h0v0v42.43H73.12V82.09H49.49v29.97H18.26V69.63L18.26,69.63L18.26,69.63z"></path>
+                    </g>
+                </svg>
+            </a>
             <a href="/charts/" sveltekit:prefetch>Charts</a>
             <a href="/tokens/" sveltekit:prefetch>Tokens</a>
         </div>
