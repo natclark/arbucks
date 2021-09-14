da91a7f4e80f518bbdf0da039de104f0c79e6fe8
natclark <39421782+natclark@users.noreply.github.com>
2021-09-14 15:40:29 -0500

    increase ledger range

---
+++ src/routes/blog/index.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+    <title>Blog - Arbucks</title>
+    <link rel="canonical" href="https://arbucks.io/blog/">
+    <meta property="og:title" content="Blog - Arbucks">
+    <meta name=twitter:title content="Blog - Arbucks">
+</svelte:head>
+
+<h1>Blog</h1>
